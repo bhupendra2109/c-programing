f2101ce18d38d1fcc224be49f56113a907f88591
bhupendra2109 <bhupendrachauhan11k@gmail.com>
2025-08-28 15:29:31 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+
+int a=27;
+int b=95;
+int c=55;
+float simple_interest=(a*b*c)/100;
+printf("%d",a);
+printf("%d",b);
+printf("%d",c);
+printf("%f",simple_interest);
+return 0;
+}
+
+
+
+
+
---
+++ divide.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+  float main(){
+  float a,b,c;
+
+  printf("the value of a= \n");
+  scanf("%f",&a);
+  printf("the value of b= \n");
+  scanf("%f",&b);
+
+  c=a/b;
+
+printf("c=%f",c);
+return 0;
+  }
