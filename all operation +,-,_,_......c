4ebe6a5c1f7d4c9964138339003b3ca7b7dd2fe1
bhupendra2109 <bhupendrachauhan11k@gmail.com>
2025-08-28 23:18:16 +0530

    Add files via upload

---
+++ all operation +,-,_,_......c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+float main()
+{
+float a,b;
+printf("Enter the first number : ");
+scanf("%f",&a);
+printf("Enter the second number : ");
+scanf("%f",&b);
+printf("\nThe sum is %f. ",a+b);
+printf("\nThe difference is %f. ",a-b);
+printf("\nThe product is %f. ",a*b);
+printf("\nThe devide is %f. ",a/b);
+return 0;
+}
