5eee3cce8e83922c262615a01c20920f3933f4ad
bhupendra2109 <bhupendrachauhan11k@gmail.com>
2025-08-28 21:25:15 +0530

    Add files via upload

---
+++ convert hours into minutes.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+
+int main() {
+    int min,hour;
+
+
+    printf("enter the time of  hour= \n" );
+    scanf("%d",&hour);
+min=hour*60;
+    printf("ans min= %d",min);
+
+    return 0;
+}
+
---
+++ convert min into hours.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+
+float main() {
+    float min,hour;
+
+
+    printf("enter the time of  min= \n" );
+    scanf("%f",&min);
+hour=min/60;
+    printf("ans hours= %f",hour);
+
+    return 0;
+}
+
