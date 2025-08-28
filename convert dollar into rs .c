0e2c01de800147e50d9fd5221811b050ae83eed9
bhupendra2109 <bhupendrachauhan11k@gmail.com>
2025-08-28 21:42:01 +0530

    Add files via upload

---
+++ convert dollar into rs .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+    int dollar,Rs;
+
+
+    printf("Enter the money of dollar= \n" );
+    scanf("%d",&dollar);
+
+    Rs=dollar*48;
+
+  
+    printf("ans Rs= %d",Rs);
+
+    return 0;
+}
+
